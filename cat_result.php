649ca20fc45d28407d2ce163d1470485bf0cebc2
CX <CX>
2016-03-25 19:08:26 +0800

    Started on the categories subpage

--- cat_result.php
+++ cat_result.php
@@ -20,46 +20,6 @@ $dbcon = pg_connect("host=$host dbname=$db user=$user password=$pass")
     or die('Could not connect: ' . pg_last_error());
 ?>
 
-<script type="text/javascript" src="js/jquery.js"></script>
-
-<script type="text/javascript">
-  jQuery(window).scroll(function (event) {
-	  	
-		var top = jQuery('#popular-upcoming').offset().top - jQuery(document).scrollTop();;
-		// what the y position of the scroll is
-		var y = jQuery(this).scrollTop();
-		// whether that's below the form
-		if (y >= top)  {
-		// if so, add the active class to popular-upcoming and remove from content
-		jQuery('.page-nav-popular-posts').addClass('active');
-		jQuery('.page-nav-top-posts').removeClass('active');
-		} else {
-		// otherwise remove it
-		jQuery('.page-nav-popular-posts').removeClass('active');
-		jQuery('.page-nav-top-posts').addClass('active');
-	   }
-  });
-  
-  jQuery(document).ready(function (){
-  jQuery('#popular-scroll').click(function (){
-            //jQuery(this).animate(function(){
-                jQuery('html, body').animate({
-                    scrollTop: jQuery('#popular-upcoming').offset().top
-                     }, 2000);
-            //});
-        });
-		
-		jQuery('#feature-scroll').click(function (){
-            //jQuery(this).animate(function(){
-                jQuery('html, body').animate({
-                    scrollTop: jQuery('#inner').offset().top
-                     }, 2000);
-            //});
-        });
-		  });
-	  </script>
-</head>
-
 <body class="home blog header-full-width full-width-content">
   <div id="header">
   <div class="site-header">
@@ -69,9 +29,9 @@ $dbcon = pg_connect("host=$host dbname=$db user=$user password=$pass")
     </h1>
       
             <ul id="page-nav" class="horizontal-list">
-<li class="page-nav-top-posts active"><a href="javascript:void(0)" id="feature-scroll" class="page-anchor-link">Home</a></li>
+<li class="page-nav-top-posts active"><a href="index.php" id="feature-scroll" class="page-anchor-link">Home</a></li>
 
-<li class="page-nav-popular-posts"><a href="javascript:void(0)" id="popular-scroll" class="page-anchor-link">Most Popular</a></li>
+<li class="page-nav-popular-posts"><a href="index.php#popular-upcoming" id="popular-scroll" class="page-anchor-link">Most Popular</a></li>
 
 <li class="page-nav-top-posts active"><a href="index.php#categories" id="feature-scroll" class="page-anchor-link">Categories</a></li>
 
@@ -87,7 +47,7 @@ $dbcon = pg_connect("host=$host dbname=$db user=$user password=$pass")
 		<li id="menu-item-142" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-142"><a href="sample-page.htm">Host Project</a></li>
 	</ul></div></div><!-- #site-nav -->
 <div id="site-header-bigg-social">
-    
+     
     </div>  
     </div>
   </div>
@@ -95,19 +55,9 @@ $dbcon = pg_connect("host=$host dbname=$db user=$user password=$pass")
 <div id="inner">
 <div class="wrap">
 <div id="content-sidebar-wrap">
-				
-				<div id="content" class="hfeed">
-				<div class="post-5 post type-post status-publish format-standard hentry category-featured category-parent-category-i entry feature feature">
-
-		<a href="#" title="Welcome to Uptown Fund"><img width="660" height="370" src="images/1.jpg" class="alignleft post-image" alt="1" /></a>		<h2 class="entry-title"><a href="#" title="Welcome to Uptown Fund" rel="bookmark">Welcome to Uptown Fund</a></h2> 
 		
-				<div class="entry-content">
-			<p>Uptown Fund is the one-stop hub to turn ideas into successful inventions.  Here, you can reach out to potential investors by hosting your projects and ideas.  Alternatively, you can browse through and contribute to thousands of new inventions from all around the world. </p>
-
-			<a href="index.php#cat" class="bigg-read-more">Browse</a>		</div><!-- end .entry-content -->
-		
-	</div><!-- end .postclass -->
-	</div><!-- end #content -->
+</div>end .postclass
+</div><!-- end #content -->
 
 <!-- Categories Section -->
 <div id="cat" class="categories">
@@ -117,10 +67,30 @@ $dbcon = pg_connect("host=$host dbname=$db user=$user password=$pass")
 </div>
 <br>
 
-<!-- Categories List -->
-<div class="categories">
-	<a href="cat_result.php?varname=<?php echo "art" ?>"><span><img src="images/art.jpg" style="width: 100%" class="post-image">Art</span></a>
-	<a href="cat_result.php?varname=<?php echo "education" ?>"><span><img src="images/education.jpg" style="width: 100%" class="post-image">Education</span></a>
+<!-- fetch the variable name -->
+<?php $var_value = $_GET['varname'];?>
+
+<?php echo $var_value;?>
+
+
+<ul class="plain-list stories-table">    
+<?php
+$query = 'SELECT p.title FROM project p, has_category h WHERE p.id=h.id AND h.tag=$var_value ORDER BY p.title';
+$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+?>
+
+<?php while($line = pg_fetch_array($result, null, PGSQL_ASSOC)){ ?>
+	<?php foreach ($line as $col_value) { ?>
+  	<li><a href="#"><?php echo $col_value; ?></a></li>
+   <?php } ?>
+ <?php } ?>
+<?php pg_free_result($result); ?>
+</ul>
+
+
+<!-- <div class="categories">
+	<a href="index.html"><span><img src="images/art.jpg" style="width: 100%" class="post-image">Art</span></a>
+	<a href="index.html"><span><img src="images/education.jpg" style="width: 100%" class="post-image">Education</span></a>
 	<a href="index.html"><span><img src="images/environment.jpg" style="width: 100%" class="post-image">Environment</span></a>
 	<a href="index.html"><span><img src="images/game.jpg" style="width: 100%" class="post-image">Gaming</span></a>
 
@@ -128,52 +98,9 @@ $dbcon = pg_connect("host=$host dbname=$db user=$user password=$pass")
 	<a href="index.html"><span><img src="images/technology.jpg" style="width: 100%" class="post-image">Technology</span></a>
 	<a href="index.html"><span><img src="images/video.jpg" style="width: 100%" class="post-image">Video</span></a>
 	<a href="index.html"><span><img src="images/other.jpg" style="width: 100%" class="post-image">Others</span></a>
-</div>
+</div> -->
 
-<!-- Popular List -->
-<div id="popular-upcoming" class="stories-container sixcol">
 
-<div class="stories-section-header">
-<h2 id="popular" class="stories-section-header-hed">Popular</h2>
-<h3 class="stories-section-header-subhed">The most popular crowdfunding projects</h3>
-</div>
-
-<ul class="plain-list stories-table">    
-<?php
-$query = 'SELECT p.title FROM donation d, project p WHERE p.id=d.project GROUP BY p.title ORDER BY COUNT(d.project) DESC LIMIT 10';
-
-$result = pg_query($query) or die('Query failed: ' . pg_last_error());
-?>
-
-<?php while($line = pg_fetch_array($result, null, PGSQL_ASSOC)){ ?>
-	<?php foreach ($line as $col_value) { ?>
-  	<li><a href="#"><?php echo $col_value; ?></a></li>
-   <?php } ?>
- <?php } ?>
-<?php pg_free_result($result); ?>
-</ul>
-</div>
-
-<!-- Countries List -->
-<div id="countries" class="stories-container sixcol last">
-<div class="stories-section-header">
-<h2 class="stories-section-header-hed">Countries</h2>
-<h3 class="stories-section-header-subhed">Browse projects by countries</h3>
-</div>
-
-<ul class="plain-list stories-table">    
-<?php
-$query = 'SELECT DISTINCT country FROM project ORDER BY country';
-$result = pg_query($query) or die('Query failed: ' . pg_last_error());
-?>
-
-<?php while($line = pg_fetch_array($result, null, PGSQL_ASSOC)){ ?>
-	<?php foreach ($line as $col_value) { ?>
-  	<li><a href="#"><?php echo $col_value; ?></a></li>
-   <?php } ?>
- <?php } ?>
-<?php pg_free_result($result); ?>
-</ul>
 
 
 	</div><!-- end #content-sidebar-wrap -->
