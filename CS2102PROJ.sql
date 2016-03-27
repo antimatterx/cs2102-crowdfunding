45b882ae5d816145adac259849cbdefc90a8fb80
Acrobat622 <86.ed.liu@gmail.com>
2016-03-28 01:12:40 +0800

    some spelling errors in the sql scripts i am not sure about the value of 'mark_twain@gmail.com' in project field, so i change it to some other existing mail address

--- CS2102PROJ.sql
+++ CS2102PROJ.sql
@@ -65,7 +65,7 @@ INSERT INTO project (id, creator, title, description, start, expiry, country, ta
 INSERT INTO project (id, creator, title, description, start, expiry, country, target, status) VALUES (56, 'april_foo@hotmail.com', 'Learn Fast', 'Learn without barriers with our special tool', '2016/02/01', '2016/04/27', 'Singapore', 650, 'ongoing');
 INSERT INTO project (id, creator, title, description, start, expiry, country, target, status) VALUES (67, 'john_doe@hotmail.com', 'Waste Less', 'Reduce waste using this novel invention', '2016/01/01', '2016/06/30', 'Indonesia', 1500, 'ongoing');
 INSERT INTO project (id, creator, title, description, start, expiry, country, target, status) VALUES (73, 'sam_loh@yahoo.com', 'Speed Mouse', 'Control the cursor like never before', '2016/02/08', '2016/03/08', 'USA', 200, 'closed');
-INSERT INTO project (id, creator, title, description, start, expiry, country, target, status) VALUES (84, 'mark_twain@hotmail.com', 'High Res Camera', 'Take videos with 16k resolution', '2016/03/10', '2016/07/31', 'Britain', 100000, 'ongoing');
+INSERT INTO project (id, creator, title, description, start, expiry, country, target, status) VALUES (84, 'john_doe@hotmail.com', 'High Res Camera', 'Take videos with 16k resolution', '2016/03/10', '2016/07/31', 'Britain', 100000, 'ongoing');
 INSERT INTO project (id, creator, title, description, start, expiry, country, target, status) VALUES (72, 'sarah_lee@gmail.com', 'Digital Score', 'Scan and playback sheet music!', '2016/02/13', '2016/10/13', 'USA', 20000, 'ongoing');
 INSERT INTO project (id, creator, title, description, start, expiry, country, target, status) VALUES (14, 'able_too@gmail.com', 'Food Maker', 'Cook twice the amount of food in half the time!', '2015/09/14', '2016/10/14', 'China', 3500, 'ongoing');
 
@@ -78,10 +78,10 @@ INSERT INTO donation (time, amount, donor, project) VALUES ('2015-10-23 12:12:12
 INSERT INTO donation (time, amount, donor, project) VALUES ('2015-10-03 00:00:01', 1, 'apple_on_my_head@hotmail.com', 567);
 INSERT INTO donation (time, amount, donor, project) VALUES ('2016-02-02 10:10:10', 99, 'molly_miller@gmail.com', 67);
 INSERT INTO donation (time, amount, donor, project) VALUES ('2016-03-01 00:00:01', 5000, 'mary_john@gmail.com', 55);
-INSERT INTO donation (time, amount, donor, project) VALUES ('2016-02-03 12:12:12', 23, 'sam_loh@gmail.com', 67);
-INSERT INTO donation (time, amount, donor, project) VALUES ('2015-10-03 00:00:01', 1, 'sarah_lee@hotmail.com', 567);
-INSERT INTO donation (time, amount, donor, project) VALUES ('2016-02-014 10:10:10', 5, 'john_doe@gmail.com', 72);
-INSERT INTO donation (time, amount, donor, project) VALUES ('2016-02-28 00:00:01', 400, 'zach_brown@gmail.com', 72);
+INSERT INTO donation (time, amount, donor, project) VALUES ('2016-02-03 12:12:12', 23, 'sam_loh@yahoo.com', 67);
+INSERT INTO donation (time, amount, donor, project) VALUES ('2015-10-03 00:00:01', 1, 'sarah_lee@gmail.com', 567);
+INSERT INTO donation (time, amount, donor, project) VALUES ('2016-02-014 10:10:10', 5, 'john_doe@hotmail.com', 72);
+INSERT INTO donation (time, amount, donor, project) VALUES ('2016-02-28 00:00:01', 400, 'zack_brown@gmail.com', 72);
 
 
 --CATEGORY
