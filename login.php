ce95d382bb2dd86369feb2beeba8b2bc17aeded3
CX <CX>
2016-03-28 01:02:28 +0800

    removed extraneous stuff in login.php

--- login.php
+++ login.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
    $host = "localhost"; 
    $user = "postgres"; 
@@ -79,31 +78,5 @@
 
    </body>
 </html>
-<?php
-session_start();
 
-$username = $_POST['username'];
-$password = $_POST['password'];
-
-$host = "localhost";
-$user = "postgres";
-$pass = "password";
-$db = "test";
-
-$con = pg_connect("host=$host dbname=$db user=$user password=$pass")
-or die('Could not connect: ' . pg_last_error());
-
-//$query = "SELECT * FROM person WHERE name='$username' AND password='$password'";
-$query = "SELECT * FROM person WHERE name='Peter' AND password='123456'";
-$result = pg_query($con, $query)or die(pg_last_error());
-$num_row = pg_num_rows($result);
-		$row=pg_fetch_array($result);
-		if( $num_row ==1 ) {
-			echo 'true';
-			$_SESSION['user_name']=$row['name'];			
-			
-		}
-		else{
-			echo "$username";
-		}
 ?>
