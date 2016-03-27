059573b6ceed5f0c07768c3cc8e64a2d826dff05
Acrobat622 <86.ed.liu@gmail.com>
2016-03-27 23:10:16 +0800

    a simple test for forms that i have implemented, please take a look and give it a try the form_test shall tell you what you have entered the txt file is the link that i consulted and followed, but never works

---
+++ phpformtest/form.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head></head>
+<body>
+<div class="content">
+    <h1>Login to Web App</h1>
+    <form method="POST" action="form_test.php">
+        <p><input type="text" name="username" value="" placeholder="Username"></p>
+        <p><input type="password" name="password" value="" placeholder="Password"></p>
+        <input type="submit" name="login" value="Login">
+    </form>
+</div>
+</body>
+</html>
