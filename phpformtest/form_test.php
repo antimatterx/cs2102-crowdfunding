e504805b869c012b8623aea140f409f9aed5d0e6
Acrobat622 <86.ed.liu@gmail.com>
2016-03-27 23:15:40 +0800

    removed form tests

--- phpformtest/form_test.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    echo "You have entered:<br/>";
-    echo "Username $username<br/>";
-    echo "Password $password<br/>";
-?>
